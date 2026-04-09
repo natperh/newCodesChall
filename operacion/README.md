76cbd4c7ee0dced057374d2f707bc14e4146529e
natperh <115506506+natperh@users.noreply.github.com>
2026-04-08 19:01:09 -0600

    Delete operacion directory

--- operacion/README.md
+++
@@ -1,3 +0,0 @@
-# operacion
-- [Wiki y Fidelidad](./docs/README.md)
-- [Código](./operacion/)
--- operacion/operacion/src/main/java/com/bbva/modernizer/ModernizedService.java
+++
@@ -1,7 +0,0 @@
-```java
-public class SumaService {
-    public int sumar(int num1, int num2) {
-        return num1 + num2;
-    }
-}
-```
