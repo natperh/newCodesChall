7109a611a2b210f66963f46c93f1b3b51b2d0c53
natperh <115506506+natperh@users.noreply.github.com>
2026-04-09 22:24:14 -0600

    Delete operacion directory

--- operacion/operacion/src/main/java/com/bbva/modernizer/ModernizedService.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public record Suma(int num1, int num2) {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int resultado = num1 + num2;
-    }
-}
